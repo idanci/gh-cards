0d9eff2809842688ac8048dcb21eeb9198c1c775
Ivan Danci <bah4uk@gmail.com>
2019-12-22 23:39:30 +0200

    Add state to Form

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
 class Form extends React.Component {
+  state = {
+    userName: ''
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.userName);
+  }
+
   render() {
     return (
-      <form action="">
-        <input type="text" placeholder="GitHub username"/>
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="GitHub username"
+          value={this.state.userName}
+          onChange={event => this.setState({userName: event.target.value})}
+          required
+        />
         <button>Add card</button>
       </form>
     );
