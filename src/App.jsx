01deb85acf0b9cab5dfa3167a6dede5c26bc7eb0
Ivan Danci <bah4uk@gmail.com>
2019-12-22 23:23:42 +0200

    Add Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
 import CardList from './CardList';
+import Form from './Form';
+
+const testData = [
+  {name: "Ivan Danci", avatar_url: "https://avatars2.githubusercontent.com/u/2598910?v=4", company: "Endava"},
+  {name: "David Heinemeier Hansson", avatar_url: "https://avatars0.githubusercontent.com/u/2741?v=4", company: "Basecamp"},
+];
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-        <CardList />
+        <Form />
+        <CardList profiles={testData} />
       </div>
     );
   }
