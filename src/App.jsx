8cde259883f121d57a6e5b951ed9dffb60d120b3
Ivan Danci <bah4uk@gmail.com>
2019-12-22 23:49:48 +0200

    Fetch user profile from github

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,15 @@ class App extends React.Component {
   state = {
     profiles: testData,
   };
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({profiles: [...prevState.profiles, profileData]})),
+  };
 
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-        <Form />
+        <Form onSubmit={this.addNewProfile}/>
         <CardList profiles={this.state.profiles} />
       </div>
     );
