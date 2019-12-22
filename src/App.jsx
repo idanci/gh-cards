84fc1b3677442784a08ac2b0a7acf7382536c3f1
Ivan Danci <bah4uk@gmail.com>
2019-12-22 21:50:04 +0200

    Add card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Card from './Card';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
+        <Card />
       </div>
     );
   }
